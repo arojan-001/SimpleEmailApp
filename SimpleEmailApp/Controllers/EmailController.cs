2bcc94639f695dc315c24e2f89a9fa27875d705e
arojan-001 <arayik.petrosian@mail.ru>
2022-12-23 18:00:48 +0400

    SimpleEmailApp
    Send Email with a .NET 6 Web API using Mailkit & SMTP

---
+++ SimpleEmailApp/Controllers/EmailController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleEmailApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _service;
+
+        public EmailController(IEmailService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _service.SendEmail(request);
+
+            return Ok();
+        }
+        
+    }
+}
